7c240e70dff3075140337f1221dc174ff1ff4ef4
Lim, Mike, M.S. <choonchernlim@gmail.com>
2026-07-27 08:40:03 -0500

    feat: remove ghostty references from personal and work Nix configurations

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.color_scheme = "rose-pine-moon"
+config.color_scheme = "Gruvbox Dark (Gogh)"
 config.font = wezterm.font("Hack Nerd Font")
 config.font_size = 15.0
 config.window_background_opacity = 0.8
@@ -11,5 +11,6 @@ config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
 config.initial_cols = 150
 config.initial_rows = 50
+config.window_close_confirmation = "NeverPrompt"
 
 return config
